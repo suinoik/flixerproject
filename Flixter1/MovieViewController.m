386779526988502cc7c123860400938f87a97d2e
Onwuosiuno Ikhioda <suinoik@fb.com>
2022-06-16 14:36:50 -0700

    set uo movies array

---
+++ Flixter1/MovieViewController.m
@@ -0,0 +1,50 @@
+//
+//  MovieViewController.m
+//  Flixter1
+//
+//  Created by Onwuosiuno Ikhioda on 6/16/22.
+//
+
+#import "MovieViewController.h"
+
+@interface MovieViewController ()
+
+@end
+
+@implementation MovieViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=cb02447945bd8b38db35a1609e415b14"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+           if (error != nil) {
+               NSLog(@"%@", [error localizedDescription]);
+           }
+           else {
+               NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+               NSArray *myArray = dataDictionary[@"results"];
+               NSLog(@"%@", myArray);
+
+               // TODO: Get the array of movies
+               // TODO: Store the movies in a property to use elsewhere
+               // TODO: Reload your table view data
+           }
+       }];
+    [task resume];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
